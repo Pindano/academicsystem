02c41117a2eafa94c1e90e5c0dedd9cff2566ec4
cicily19 <cicily.muendo@strathmore.edu>
2024-07-15 09:26:23 +0300

    modification of examinations table

--- database/migrations/2024_07_13_081000_create_examinations_table.php
+++ database/migrations/2024_07_13_081000_create_examinations_table.php
@@ -15,8 +15,6 @@ return new class extends Migration
     {
         Schema::create('examinations', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->nullable();
-            $table->string('term');
             $table->timestamps();
         });
     }
